e0f8df3c39d58cb9e383336ca7a6d31690124519
Christian Ulrich <christian.ulrich@tcsag.de>
2020-12-03 10:14:58 +0000

    Find works for Remote Modules

--- halc/remote.py
+++ halc/remote.py
@@ -3,15 +3,39 @@ try: import rpyc
 except: print('RPyC is not installed')
 import threading,time
 class RPyCModules(hal.Proxy):
-    def Connect(self,Host=None,Port=18878):
+    def __init__(self, id, parent=None):
+        self.Connect(id)
+        hal.Proxy.__init__(self,id,parent)
+        self.hal = None
+    def find(self,id=None,typ=None,Name=None):
+        rsys = self.conn.modules.sys
+        if self.hal is None:
+            for module in rsys.modules:
+                if 'halc.hal' in str(module):
+                    self.hal = self.conn.modules[module]
+        if self.hal is not None:
+            return self.hal.Devices.find(id,typ,Name)
+        return None
+    def Connect(self,Host=None,Port=18812):
         if Host is None:
             self.conn = rpyc.classic.connect(self._id, port=Port)
         else:
             self.conn = rpyc.classic.connect(Host, port=Port)
     def Disconnect(self):
         self.conn.close()
+    def AddPath(self,Path):
+        try:
+            rsys = self.conn.modules.sys
+            rsys.path.insert(0,Path)
+            return True
+        except:
+            return False
     def LoadModule(self,Name):
-        return self.conn.modules[Name]
+        try:
+            return self.conn.modules[Name]
+        except BaseException as e:
+            self.logger.debug(str(e))
+            return False
 class Enumerate(threading.Thread): 
     def __init__(self): 
         threading.Thread.__init__(self) 
@@ -29,7 +53,11 @@ class Enumerate(threading.Thread):
                         ip,port = service
                         adev = hal.Devices.find(ip,RPyCModules)
                         if adev == None:
-                            print(ip,port)
+                            try:
+                                dev = RPyCModules(ip)
+                                #print(ip,port)
+                            except: 
+                                del(dev)
                         else:
                             adev.Found = True
                     for dev in hal.Devices.Modules:
