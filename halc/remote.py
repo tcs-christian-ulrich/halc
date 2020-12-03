d0c1ec34014ee8cc884869b13c5d95ccce6482e1
Christian Ulrich <christian.ulrich@tcsag.de>
2020-12-03 08:22:06 +0000

    started implmenting discover

--- halc/remote.py
+++ halc/remote.py
@@ -1,6 +1,7 @@
 from . import hal
 try: import rpyc
 except: print('RPyC is not installed')
+import threading,time
 class RPyCModules(hal.Proxy):
     def Connect(self,Host=None,Port=18878):
         if Host is None:
@@ -11,3 +12,33 @@ class RPyCModules(hal.Proxy):
         self.conn.close()
     def LoadModule(self,Name):
         return self.conn.modules[Name]
+class Enumerate(threading.Thread): 
+    def __init__(self): 
+        threading.Thread.__init__(self) 
+    def run(self):
+        try:
+            from rpyc.utils import registry
+            registrar = registry.UDPRegistryClient()
+            while threading.main_thread().isAlive():
+                try:
+                    for dev in hal.Devices.Modules:
+                        if isinstance(dev,RPyCModules):
+                            dev.Found = False
+                    services = registrar.discover('slave')
+                    for service in services:
+                        ip,port = service
+                        adev = hal.Devices.find(ip,RPyCModules)
+                        if adev == None:
+                            print(ip,port)
+                        else:
+                            adev.Found = True
+                    for dev in hal.Devices.Modules:
+                        if isinstance(dev,RPyCModules):
+                            if dev.Found == False:
+                                del(dev)
+                    time.sleep(1)
+                except: 
+                    pass
+        except: pass
+enumerate = Enumerate() 
+enumerate.start()
