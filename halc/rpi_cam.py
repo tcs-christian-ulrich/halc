4e206cc53eb0a31ef30297b26ee5ada3703824aa
Christian U <christian@ullihome.de>
2021-05-24 17:06:42 +0200

    cv2 and rpicam optimized

--- halc/rpi_cam.py
+++ halc/rpi_cam.py
@@ -1,7 +1,6 @@
 from . import hal
 try:
-    from picamera import PiCamera
-    import cv2
+    import cv2,threading,io,time,picamera,picamera.array
     import numpy as np
     class PICamera(hal.Camera):
         def __init__(self, port=-1, parent=None,Name=None):
@@ -11,24 +10,20 @@ try:
                 hal.Sensor.__init__(self,str(port),parent)
             self.Port = port
             self.Name = Name
-        def read(self,CloseCapture = False):
+            self.cam = None
+        def capture(self,CloseCapture = False):
             if self.cam is None:
                 self.cam = picamera.PiCamera()
                 self.cam.start_preview()
                 time.sleep(2)
-            stream = io.BytesIO()
-            self.cam.capture(stream, format='jpeg')
-            data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-            image = cv2.imdecode(data, 1)
-            # OpenCV returns an array with data in BGR order. If you want RGB instead
-            # use the following...
-            #image = image[:, :, ::-1]
-            return image
+            with picamera.array.PiRGBArray(self.cam) as stream:
+                self.cam.capture(stream, format='bgr')
+                image = stream.array
+                return image
     class PIEnumerate(threading.Thread): 
         def __init__(self): 
             threading.Thread.__init__(self) 
         def run(self):
-            context = pyudev.Context()
             while threading.main_thread().is_alive():
                 for dev in hal.Devices.Modules:
                     if isinstance(dev,PICamera):
@@ -49,4 +44,5 @@ try:
                 time.sleep(3)
     enumerate = PIEnumerate() 
     enumerate.start()
-except: print("picamera package needs to be installed !")
+except BaseException as e: 
+    print('picamera package needs to be installed ! ('+str(e)+'"')
