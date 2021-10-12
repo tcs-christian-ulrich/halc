60628e6ce04e4fcf877b0ba69501e8a31469dfb4
Chris <ulrich@tcs-germany.de>
2021-10-12 15:44:17 +0200

    Fix Grabbing size to TV Norm

--- halc/mp_stk1160.py
+++ halc/mp_stk1160.py
@@ -9,8 +9,7 @@ class MPCamera(hal.Grabber):
         self.Port = port
         self.Name = Name
     def capture(self,dev,CloseCapture = False):
-        #os.system("ffmpeg -f video4linux2 -video_size 720x576 -i /dev/video0 -vframes 1  /tmp/00000001.png -y")        
-        os.system("mplayer tv:// -tv driver=v4l2:device=/dev/video"+str(self.Port)+":norm=PAL:fps=25 -hardframedrop -rawvideo pal -vf pp=lb -frames 1 -vo png")
+        os.system("mplayer tv:// -tv driver=v4l2:device=/dev/video"+str(self.Port)+":norm=PAL:width=768:height=576:fps=25 -hardframedrop -rawvideo pal -vf pp=lb -frames 1 -vo png")
         img = cv2.imread("00000001.png")
         if self.logger.getEffectiveLevel() < logging.DEBUG:
             os.remove("00000001.png")
